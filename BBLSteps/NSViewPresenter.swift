8d8e532ce86c92f7f11aa6c6e52edd3da9a999cf
Andy Park <andy@bigbearlabs.com>
2017-01-30 09:12:44 +0100

    place reusables in framework; extract the main use case out to client project and revise it into an example here.

---
+++ BBLSteps/NSViewPresenter.swift
@@ -0,0 +1,112 @@
+//
+//  NSViewPresenter.swift
+//  BBLSteps
+//
+//  Created by ilo on 30/01/2017.
+//  Copyright © 2017 Big Bear Labs. All rights reserved.
+//
+
+import Foundation
+
+
+
+public class NSViewPresenter: Presenter {
+  
+  var currentViewController: NSViewController!
+  
+  let contentView: NSView
+  
+  public init(contentView: NSView) {
+    self.contentView = contentView
+  }
+  
+  public func present(_ step: Step) {
+    // make a view and add to the content view.
+    self.currentViewController = viewControllerFor(step)
+    
+    contentView.removeAllSubviews()
+    contentView.addSubview(self.currentViewController.view, fit: true)
+    
+  }
+  
+  func viewControllerFor(_ step: Step) -> NSViewController {
+    let vc = StepViewController(nibName: "StepViewController", bundle: Bundle.init(for: type(of: self)))
+    vc!.step = step
+    return vc!
+  }
+}
+
+
+class StepViewController: NSViewController {
+  
+  @IBOutlet weak var optionsArea: NSStackView!
+  
+  var step: Step! {
+    didSet {
+      _ = self.view
+      
+      // make controls for the options and show them.
+      self.optionsArea.removeAllSubviews()
+      self.buttonHolders = []
+      
+      self.step.availableOptions.reversed().forEach { (k, v) in
+        let optionView = optionViewFor((k, v))
+        optionsArea.addArrangedSubview(optionView)
+      }
+    }
+  }
+  
+  // bindings-compatible properties.
+  var label: String {
+    return self.step.label
+  }
+  
+  func optionViewFor(_ option: (String, ()->()) ) -> NSView {
+    let view = NSButton(frame: CGRect(x: 0, y: 0, width: 80, height: 20))
+    view.title = option.0
+    let buttonHolder = ButtonHolder(button: view) {
+      option.1()
+    }
+    self.buttonHolders.append(buttonHolder)
+    return view
+  }
+  
+  var buttonHolders: [ButtonHolder] = []
+}
+
+
+@objc
+class ButtonHolder: NSObject {
+  let button: NSButton
+  
+  var onButtonPress: ()->()
+  
+  init(button: NSButton, onPress: @escaping ()->()) {
+    self.button = button
+    self.onButtonPress = onPress
+    super.init()
+    
+    button.target = self
+    button.action = #selector(actionButtonPressed(_:))
+    
+    
+  }
+  
+  func actionButtonPressed(_ sender: Any) {
+    onButtonPress()
+  }
+}
+
+extension NSView {
+  
+  func addSubview(_ subview: NSView, fit: Bool) {
+    self.addSubview(subview)
+    if fit {
+      subview.frame = self.bounds
+    }
+  }
+  
+  func removeAllSubviews() {
+    self.subviews.forEach { $0.removeFromSuperview() }
+  }
+}
--- BBLStepsExample/AppDelegate.swift
+++ BBLStepsExample/AppDelegate.swift
@@ -18,119 +18,81 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     
+    // use case 1: interacting through the console.
 //    let intro = IntroWorkflow(presenter: ConsolePresenter())
 //    intro.present()
 
+    // use case 2: interacting through a macOS presenter.
     let intro = IntroWorkflow(presenter: NSViewPresenter(contentView: window.contentView!))
     intro.present()
   }
 
-  func applicationWillTerminate(_ aNotification: Notification) {
-    // Insert code here to tear down your application
-  }
-
-
 }
 
 
 
-class NSViewPresenter: Presenter {
+// an example sequence of steps that walks the user through highlights of the app.
+class IntroWorkflow: StepSequence {
   
-  var currentViewController: NSViewController!
-  
-  let contentView: NSView
-  
-  init(contentView: NSView) {
-    self.contentView = contentView
-  }
-  
-  func present(_ step: Step) {
-    // make a view and add to the content view.
-    self.currentViewController = viewControllerFor(step)
+  init(presenter: Presenter) {
+    var workflow: IntroWorkflow!  // to be set to self so the closures passed to super.init can reference the instance.
     
-    contentView.removeAllSubviews()
-    contentView.addSubview(self.currentViewController.view, fit: true)
+    super.init(
+      steps: [
+        Step(
+          label: "Welcome!",
+          options: [
+            "next": {
+              workflow.goNext()
+            }
+          ]
+        ),
+        Step(
+          label: "What makes me so great?",
+          options: [
+            "next": {
+              workflow.goNext()
+              workflow.expectTrialMoveCompleted() {
+                // enable the 'next' control only when the move has been made successfully.
+                workflow.enable(option: "next")
+              }
+            },
+            "previous": {
+              workflow.goPrevious()
+            }
+          ]
+        ),
+        Step(
+          label: "Try this move!",
+          options: [
+            "next": {
+              workflow.goNext()
+            },
+            "previous": {
+              workflow.goPrevious()
+            }
+          ],
+          initiallyEnabled: [
+            "previous",
+            ]
+        ),
+        Step(
+          label: "Thank you, and good luck.",
+          options: [
+            "done": {
+              workflow.finish()
+            },
+          ]
+        ),
+        ],
+      presenter: presenter)
     
+    workflow = self
   }
   
-  func viewControllerFor(_ step: Step) -> NSViewController {
-    let vc = StepViewController(nibName: "StepViewController", bundle: Bundle.init(for: type(of: self)))
-    vc!.step = step
-    return vc!
-  }
-}
-
-
-class StepViewController: NSViewController {
-  
-  @IBOutlet weak var optionsArea: NSStackView!
-  
-  var step: Step! {
-    didSet {
-      _ = self.view
-      
-      // make controls for the options and show them.
-      self.optionsArea.removeAllSubviews()
-      self.buttonHolders = []
-      
-      self.step.availableOptions.reversed().forEach { (k, v) in
-        let optionView = optionViewFor((k, v))
-        optionsArea.addArrangedSubview(optionView)
-      }
-    }
-  }
-  
-  // bindings-compatible properties.
-  var label: String {
-    return self.step.label
-  }
-  
-  func optionViewFor(_ option: (String, ()->()) ) -> NSView {
-    let view = NSButton(frame: CGRect(x: 0, y: 0, width: 80, height: 20))
-    view.title = option.0
-    let buttonHolder = ButtonHolder(button: view) { 
-      option.1()
-    }
-    self.buttonHolders.append(buttonHolder)
-    return view
-  }
-  
-  var buttonHolders: [ButtonHolder] = []
-}
-
-
-@objc
-class ButtonHolder: NSObject {
-  let button: NSButton
-  
-  var onButtonPress: ()->()
-  
-  init(button: NSButton, onPress: @escaping ()->()) {
-    self.button = button
-    self.onButtonPress = onPress
-    super.init()
+  func expectTrialMoveCompleted(onComplete: ()->()) {
+    // TODO perform the magic that detects the move, then ...
     
-    button.target = self
-    button.action = #selector(actionButtonPressed(_:))
-    
-    
-  }
-  
-  func actionButtonPressed(_ sender: Any) {
-    onButtonPress()
-  }
-}
-
-extension NSView {
-  
-  func addSubview(_ subview: NSView, fit: Bool) {
-    self.addSubview(subview)
-    if fit {
-      subview.frame = self.bounds
-    }
-  }
- 
-  func removeAllSubviews() {
-    self.subviews.forEach { $0.removeFromSuperview() }
+    onComplete()
   }
 }
